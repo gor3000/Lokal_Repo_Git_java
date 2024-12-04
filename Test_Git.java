7ef27faefcfe75c5fea3c1a90c6955501cabfa8b
Spring_Repo_2 <Spring_Repo_2@saxalxo.com>
2024-12-04 12:07:34 +0100

    Implement add function java 02

--- Test_Git.java
+++ Test_Git.java
@@ -1,6 +1,13 @@
 public class Test_Git {
     public static void main(String[] args) {
+        Test_Git C = new Test_Git();
+        int c=Test_Git.add(10,50);
 
         System.out.println("Hello World");
+        System.out.println("a+b: "+c);
+    }
+    private static int add(int a, int b) {
+
+        return a + b;
     }
 }
