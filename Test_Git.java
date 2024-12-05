8d02b1ec6fa357504713f2d53463f77320e9ffed
Spring_Repo_2 <Spring_Repo_2@saxalxo.com>
2024-12-05 09:26:45 +0100

    Implement mod function java 04

--- Test_Git.java
+++ Test_Git.java
@@ -1,22 +1,31 @@
 public class Test_Git {
     public static void main(String[] args) {
 
-        int a=50;
-        int b=10;
+        int a = 50;
+        int b = 10;
 
 
-
-        System.out.println(add(a,b));
-        System.out.println(sub(a,b));
-        System.out.println(mul(a,b));
+        System.out.println(add(a, b));
+        System.out.println(sub(a, b));
+        System.out.println(mul(a, b));
+        System.out.println(div(a, b));
     }
+
     private static int add(int a, int b) {
         return a + b;
     }
+
     private static int sub(int a, int b) {
         return a - b;
     }
+
     private static int mul(int a, int b) {
         return a * b;
+    }
+
+    private static int div(int a, int b) {
+        return a / b;
+
+
     }
 }
